634cbd04a47b6ff40a248e4f25b18f9f93b648c3
Prarthana-git <prarthanachaudhari@gmail.com>
2021-05-26 21:38:22 +0530

    Added swap program

---
+++ src/com/bridgelabz/PowerOfTwo.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+import java.util.*;
+
+public class PowerOfTwo {
+
+	public static void main(String[] args) {
+		Scanner p = new Scanner(System.in);
+		System.out.println("Enter number");
+		int n = p.nextInt();//user will enter the number 
+		int poweroftwo = 1;
+		for (int i = 0; i < n; i++) {
+			System.out.println(i + " " + poweroftwo); // print out the power of two
+			poweroftwo = 2 * poweroftwo; // double to get the next one
+		}
+	}
+
+}
---
+++ src/com/bridgelabz/Vowels.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+import java.util.*;
+
+public class Vowels {
+
+	public static void main(String[] args) {
+
+		Scanner Ch = new Scanner(System.in);
+		System.out.println("Enter Character");
+		//store character in c index value is 0
+		char c = Ch.next().charAt(0);
+		if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+			System.out.println("Given character is an vowel");
+		} else {
+			System.out.println("Given character is a consonant");
+		}
+	}
+
+}
